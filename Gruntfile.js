8156f0ddbab966614c899cadfc80deafefb96b72
Marcelo Lazaroni <lazaronijunior@gmail.com>
2016-03-14 11:18:36 +0000

    Add more boilerplate

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,11 +6,62 @@ module.exports = function (grunt) {
         path: './demo/index.html',
       },
     },
+    concat: {
+      dist: {
+        src: ['src/*.js'],
+        dest: 'dist/fl-form-builder.js',
+      },
+    },
+    sass: {
+      dist: {
+        options: {
+          style: 'expanded',
+        },
+        files: {
+          'dist/fl-form-builder.css': 'src/**/*.scss',
+        },
+      },
+    },
+    uglify: {
+      main: {
+        options: {
+          sourceMap: true,
+          sourceMapName: 'dist/fl-form-builder.map',
+        },
+        files: {
+          'dist/fl-form-builder.min.js': ['dist/fl-form-builder.js'],
+        },
+      },
+    },
+    watch: {
+      css: {
+        files: 'src/**/*.sass',
+        tasks: ['css-build'],
+        options: {
+          livereload: true,
+        },
+      },
+      js: {
+        files: 'src/**/*.js',
+        tasks: ['js-build'],
+        options: {
+          livereload: true,
+        },
+      },
+    },
   });
 
   grunt.loadNpmTasks('grunt-open');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
 
+  grunt.registerTask('default', []);
   grunt.registerTask('demo', ['open']);
-  grunt.registerTask('default', ['jshint']);
+  grunt.registerTask('js-build', ['concat', 'uglify']);
+  grunt.registerTask('css-build', ['sass']);
+  grunt.registerTask('build', ['js-build', 'css-build']);
+  grunt.registerTask('dev', ['build', 'watch', 'open']);
 
 };
