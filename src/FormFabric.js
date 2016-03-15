0e38cff42211e3c6d9f2218fe9332c7511d8f18b
Marcelo Lazaroni <lazaronijunior@gmail.com>
2016-03-15 18:18:26 +0000

    Make add buttons with icons

--- src/FormFabric.js
+++ src/FormFabric.js
@@ -11,11 +11,11 @@ function FormFabric(formBody) {
   }
 
   var formComponents = [
-    { desc: 'Radio buttons', constr: RadioBtns },
-    { desc: 'Checkboxes', constr: Checkboxes },
-    { desc: 'Text box', constr: TextBox },
-    { desc: 'Text area', constr: TextArea },
-    { desc: 'Dropdown', constr: Dropdown },
+    { desc: 'Radio buttons', constr: RadioBtns, class: 'glyphicon glyphicon-ok-circle' },
+    { desc: 'Checkboxes', constr: Checkboxes, class: 'glyphicon glyphicon-check' },
+    { desc: 'Dropdown', constr: Dropdown, class: 'glyphicon glyphicon-collapse-down' },
+    { desc: 'Text box', constr: TextBox, class: 'glyphicon glyphicon-text-width' },
+    { desc: 'Text area', constr: TextArea, class: 'glyphicon glyphicon-text-height' },
   ];
 
   function createElement(Constr, formBody) {
@@ -36,38 +36,46 @@ function FormFabric(formBody) {
    * @return {HTMLElement} The dropdown menu
    */
   function createOptions() {
-    var select = document.createElement('select');
-    select.classList.add('form-control');
-    select.style.display = 'inline';
-    select.style.minWidth = '0';
-    select.style.maxWidth = '200px';
-
+    // var select = document.createElement('select');
+    // select.classList.add('form-control');
+    // select.style.display = 'inline';
+    // select.style.minWidth = '0';
+    // select.style.maxWidth = '200px';
+    var frag = document.createDocumentFragment();
     formComponents.forEach(function (component, idx) {
-      var op = document.createElement('option');
+      var op = document.createElement('button');
       op.setAttribute('value', idx);
-      op.innerText = component.desc;
-      select.appendChild(op);
+      op.className = component.class;
+      op.classList.add('btn');
+      op.classList.add('btn-default');
+      op.addEventListener('click', function (e) {
+        var idx = op.value;
+        console.log('Create a ', formComponents[idx].desc);
+        createElement(formComponents[idx].constr, formBody);
+      });
+
+      frag.appendChild(op);
     });
 
-    return select;
+    return frag;
   }
 
   this.init = function init() {
     this.element = document.createElement('div');
     this.element.classList.add('fl-form-fabric');
     var options = createOptions();
-
-    var addBtn = document.createElement('button');
-    addBtn.classList.add('btn');
-    addBtn.classList.add('slight-margin-right');
-    addBtn.innerText = 'Add';
-    addBtn.addEventListener('click', function () {
-      var idx = options.selectedIndex;
-      console.log('Create a ', formComponents[idx].desc);
-      createElement(formComponents[idx].constr, formBody);
-    });
-
-    this.element.appendChild(addBtn);
+    //
+    // var addBtn = document.createElement('button');
+    // addBtn.classList.add('btn');
+    // addBtn.classList.add('slight-margin-right');
+    // addBtn.innerText = 'Add';
+    // addBtn.addEventListener('click', function () {
+    //   var idx = options.selectedIndex;
+    //   console.log('Create a ', formComponents[idx].desc);
+    //   createElement(formComponents[idx].constr, formBody);
+    // });
+    //
+    // this.element.appendChild(addBtn);
     this.element.appendChild(options);
   };
 
