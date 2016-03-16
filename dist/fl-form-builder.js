72fe3f881a3c4e4cb88dca55d367451aff0580a5
Marcelo Lazaroni <lazaronijunior@gmail.com>
2016-03-16 17:44:39 +0000

    Add padding to x-div

--- dist/fl-form-builder.js
+++ dist/fl-form-builder.js
@@ -810,12 +810,10 @@ TextBox.prototype.setLabel = function setLabel(desc) {
 /*globals FormFabric, FormBody, xController*/
 
 xController(function flFormBuilder(xDivEl) {
-  xDivEl.innerText = "I'm working!";
-
   var formBody = new FormBody();
   var fabric = new FormFabric(formBody.element);
 
-  xDivEl.classList.add('container');
+  xDivEl.classList.add('fl-form-builder');
   xDivEl.appendChild(fabric.element);
   xDivEl.appendChild(formBody.element);
 });
--- src/controller.js
+++ src/controller.js
@@ -1,12 +1,10 @@
 /*globals FormFabric, FormBody, xController*/
 
 xController(function flFormBuilder(xDivEl) {
-  xDivEl.innerText = "I'm working!";
-
   var formBody = new FormBody();
   var fabric = new FormFabric(formBody.element);
 
-  xDivEl.classList.add('container');
+  xDivEl.classList.add('fl-form-builder');
   xDivEl.appendChild(fabric.element);
   xDivEl.appendChild(formBody.element);
 });
