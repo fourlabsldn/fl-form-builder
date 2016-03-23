72e5e65711bb7bd077f19a3792cac0a5c7071a0a
Marcelo Lazaroni <lazaronijunior@gmail.com>
2016-03-23 12:12:39 +0000

    Add more unit tests to Components

--- tests/unit/components-specs.js
+++ tests/unit/components-specs.js
@@ -5,13 +5,13 @@ componentsArray.forEach(function (compHelper) {
   'use strict';
 
   describe('A ' + compHelper.name + ' Component', function () {
-    describe('when initialised should', function () {
-      var comp;
-      var name = 'My Component';
-      beforeAll(function () {
-        comp = new window[compHelper.constructorName](name);
-      });
+    var comp;
+    var name = 'My Component';
+    beforeEach(function () {
+      comp = new window[compHelper.constructorName](name);
+    });
 
+    describe('when initialised should', function () {
       it('create a component div', function () {
         expect(comp.element).toBeDefined();
       });
@@ -57,6 +57,16 @@ componentsArray.forEach(function (compHelper) {
       });
 
       if (compHelper.hasOptions) {
+        it('have a placeHolder propperty', function () {
+          expect(comp.placeHolder).toBeDefined();
+          expect(comp.placeHolder.innerHTML).toBeDefined();
+        });
+
+        it('have an addPlaceHolder function', function () {
+          expect(comp.addPlaceHolder).toBeDefined();
+          expect(typeof comp.addPlaceHolder).toBe('function');
+        });
+
         it('have an addOption function', function () {
           expect(comp.addOption).toBeDefined();
           expect(typeof comp.addOption).toBe('function');
@@ -76,6 +86,64 @@ componentsArray.forEach(function (compHelper) {
       it('have a "required" function', function () {
         expect(comp.required).toBeDefined();
         expect(typeof comp.required).toBe('function');
+      });
+    });
+
+    describe('should with the function', function () {
+      if (compHelper.hasOptions) {
+        it('getElements return all option elements', function () {
+          var els = comp.getElements(); //Should return at least the placeholder
+          expect(els).toBeDefined();
+          expect(els.length).toBeGreaterThan(0);
+        });
+
+        it('addOption increase the number of option elements', function () {
+          comp.addOption('op1'); //This one will replace the placeholder
+          comp.addOption('op2'); //This one will be displayed together with the first
+
+          var els = comp.getElements();
+          expect(els.length).toBe(2);
+        });
+
+        it('removeOption reduce the number of option elements', function () {
+          comp.addOption('op1'); //This one will replace the placeholder
+          comp.addOption('op2'); //This one will be displayed together with the first
+          comp.removeOption();
+          comp.removeOption();
+          var els = comp.getElements();
+          expect(els.length).toBe(0);
+        });
+
+      } else {
+        it('getElements return the main input element', function () {
+          var els = comp.getElements();
+          expect(els.length).toBe(1);
+        });
+      }
+
+      describe('required', function () {
+        beforeEach(function () {
+          if (compHelper.hasOptions) {
+            comp.addOption('op1'); //Add an option just to make it 'requirable'
+          }
+        });
+
+        it('return whether the required was successfully set', function () {
+          var outcome = comp.required(true);
+          expect(outcome === true || outcome === false).toBe(true);
+        });
+
+        it('change the isRequired propperty when making it required', function () {
+          var wasSet = comp.required(true);
+          expect(wasSet).toBe(true);
+          expect(comp.isRequired).toBe(true);
+        });
+
+        it('change the isRequired propperty when making it not required', function () {
+          var wasSet = comp.required(false);
+          expect(wasSet).toBe(true);
+          expect(comp.isRequired).toBe(false);
+        });
       });
     });
   });
