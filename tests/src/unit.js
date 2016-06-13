c3d55de8bb9b08066030da58c0add6f7b60752d4
lazamar <lazaronijunior@gmail.com>
2016-06-13 15:22:44 +0100

    Add textComponent-specs

--- tests/src/unit.js
+++ tests/src/unit.js
@@ -1,5 +1,8 @@
+import viewControllerSpecs from './unit/ViewController-specs';
+viewControllerSpecs();
+
 import formComponentSpecs from './unit/FormComponent-specs';
 formComponentSpecs();
 
-import viewControllerSpecs from './unit/ViewController-specs';
-viewControllerSpecs();
+import textcomponentSpecs from './unit/TextComponent-specs';
+textcomponentSpecs();
