68f5c9e2d6f2a5e6c09e5f0c0b6cc0aba2725491
lazamar <lazaronijunior@gmail.com>
2016-10-18 17:24:33 +0100

    Snapshot testing working

---
+++ __tests__/SnapshotTesting.test.js
@@ -0,0 +1,17 @@
+/* eslint-env jasmine */
+const React = require('react');
+const renderer = require('react-test-renderer');
+const FormBuilder = require('../dist/__test-files__/FormBuilder');
+const UiSnapshots = require('./UiSnapshots');
+
+const shots = new UiSnapshots('CHECK');
+
+describe('The FormBuilder`s ui', () => {
+  it('is created correctly when empty', () => {
+    const formBuildder = renderer.create(React.createElement(FormBuilder));
+
+    expect(
+      shots.matchesSnapshot(formBuildder.toJSON(), 'test1')
+    ).toBe(true);
+  });
+});
--- __tests__/UiSnapshots.js
+++ __tests__/UiSnapshots.js
@@ -1,55 +1,113 @@
 /* eslint-disable global-require */
 const fs = require('fs');
-const assert = require('fl-assert');
-const snapshotsFileName = 'uiSnapshots.json';
+const path = require('path');
+const assert = (invariant, err) => {
+  if (!invariant) {
+    throw new Error(err);
+  }
+};
+
+// =================== GET INVOKING FILE ====================== //
+
+// Impure
+// File that invoked this script right now
+const invokingFilePath = () => {
+  const originalFunc = Error.prepareStackTrace;
+  let filename;
+  try {
+    const err = new Error();
+
+    Error.prepareStackTrace = (e, stack) => stack;
+    const currentFile = err.stack[0].getFileName();
+
+    let callerFile;
+    let needle = 0;
+    while (err.stack[needle]) {
+      callerFile = err.stack[needle].getFileName();
+      if (callerFile !== currentFile) {
+        filename = callerFile;
+        break;
+      }
+      needle++;
+    }
+  } catch (err) {
+    throw new Error('Error getting task file name');
+  }
+
+  Error.prepareStackTrace = originalFunc;
+  return filename;
+};
+
+// Return info about file that last invoked a function from this file
+const getInvokingFile = () => {
+  const address = invokingFilePath();
+  const { name, dir } = path.parse(address);
+  return {
+    address,
+    dir,
+    name,
+  };
+};
+
+// =============================  SNAPSHOT  ====================================
+
+const snapshotsFileName = () => {
+  return `${getInvokingFile().name}.snapshots.json`;
+};
+
+const snapshotsFileAddress = () => {
+  return path.join(getInvokingFile().dir, snapshotsFileName());
+};
+
+const getSnapshots = () => {
+  try {
+    return require(snapshotsFileAddress());
+  } catch (e) {
+    return {};
+  }
+};
+
+const setSnapshot = (shot, shotId) => {
+  const newSnapshot = Object.assign({}, getSnapshots(), { [shotId]: shot });
+  fs.writeFileSync(snapshotsFileAddress(), JSON.stringify(newSnapshot));
+};
+
+// =============================================================================
 
 const GLOBAL = {
-  snapshots: fs.readdirSync('./').includes('uiSnapshots.json')
-    ? require(snapshotsFileName)
-    : {},
+  MODES: {
+    UPDATE: 'UPDATE',
+    CHECK: 'CHECK',
+  },
 };
 
-const MODES = {
-  update: 'update',
-  check: 'check',
-};
 
 // =================================
 
-const setSnapshot = (shot, shotId, snaps) => {
-  return Object.assign({}, snaps, { [shotId]: shot });
-};
 
-class UiSnapshots {
+module.exports = class UiSnapshots {
   constructor(mode) {
-    assert(MODES[mode]);
-
-    this.state = {
-      mode: MODES[mode],
-    };
+    assert(GLOBAL.MODES[mode], `Invalid mode in constructor: ${mode}`);
+    this.mode = GLOBAL.MODES[mode];
+    Object.preventExtensions(this);
   }
 
-  matchesSnapshot(rawShot, matchingShotId, mode = this.state.mode) {
-    assert(MODES[mode], `Invalid mode set ${mode}`);
-    const shot = typeof rawShot === 'string' ? rawShot : JSON.stringify(rawShot);
+  matchesSnapshot(shot, matchingShotId, forceMode) {
+    const mode = forceMode || this.mode;
+    assert(GLOBAL.MODES[mode], `Invalid mode set ${mode}`);
 
-    if (mode === MODES.update) {
-      GLOBAL.snapshots = setSnapshot(shot, matchingShotId, GLOBAL.snapshots);
+    if (mode === GLOBAL.MODES.UPDATE) {
+      setSnapshot(shot, matchingShotId);
       return true;
     }
 
-    if (mode === MODES.check) {
-      assert(GLOBAL.snapshots[matchingShotId], `No snapshot registered for ${matchingShotId}`);
-      return GLOBAL.snapshots[matchingShotId] === shot;
+    if (mode === GLOBAL.MODES.CHECK) {
+      const snapshots = getSnapshots();
+      assert(snapshots[matchingShotId], `No snapshot registered for ${matchingShotId}`);
+      return JSON.stringify(snapshots[matchingShotId]) === JSON.stringify(shot);
     }
 
     assert(false, `Invalid mode: ${mode}.`);
     return false;
   }
-
-  finish() {
-    fs.writeFileSync(snapshotsFileName, JSON.stringify(GLOBAL.snapshots));
-  }
-}
-
-export default UiSnapshots;
+};
