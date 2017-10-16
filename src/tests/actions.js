2c58db8e7bf2a78d5fe4ce2c51b49b778b10dbe8
foaly-nr1 <jonny@fourlabs.co.uk>
2017-10-16 10:55:25 +0100

    Mutation testing

--- src/tests/actions.js
+++ src/tests/actions.js
@@ -17,7 +17,7 @@ describe("Action", () => {
   describe("undo", () => {
     it("returns the correct action type", () => {
       const action = undo();
-      expect(action.type).toEqual("undo");
+      expect(action.type).toEqual("undo123");
     });
   });
 
